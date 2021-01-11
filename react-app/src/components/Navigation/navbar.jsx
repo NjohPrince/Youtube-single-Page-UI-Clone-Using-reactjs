c542781b5b11dcadbca16053049fdeb472a72fac
NjohPrince <jufredprince@gmail.com>
2021-01-11 05:24:01 +0100

    added the menu icon

--- react-app/src/components/Navigation/navbar.jsx
+++ react-app/src/components/Navigation/navbar.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './navbar.css'
+import MenuIcon from '@material-ui/icons/Menu'
 
 const NavBar = () => {
   return (
     <nav className="nav-container">
       <div className="logo-text">
-          <img className="logo" src="assets/images/playbutton.png" alt="logo"/>
-          <span className="text">YouTube</span>
+        <MenuIcon className="menu-icon"/>
+        <img className="logo" src="assets/images/playbutton.png" alt="logo"/>
+        <span className="text">YouTube</span>
       </div>
     </nav>
   );
